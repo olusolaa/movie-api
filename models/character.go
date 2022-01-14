11ff5fbb0e3be63dfbcc84666f29bdd30fad14db
Olusola Alao <alaoolusolae@gmail.com>
2022-01-14 23:27:57 +0100

    add character model

---
+++ models/character.go
@@ -0,0 +1,16 @@
+package models
+
+type Character struct {
+	Name      string `json:"name"`
+	Height    string `json:"height"`
+	Mass      string `json:"mass"`
+	HairColor string `json:"hair_color"`
+	SkinColor string `json:"skin_color"`
+	EyeColor  string `json:"eye_color"`
+	BirthYear string `json:"birth_year"`
+	Gender    string `json:"gender"`
+}
+
+type CharacterLinks struct {
+	Characters []string `json:"characters"`
+}
