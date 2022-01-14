c6cfdf78a459929cf68b0c2b811ede67ff70588a
Olusola Alao <alaoolusolae@gmail.com>
2022-01-14 17:08:09 +0100

    add cache to main server file

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/olusolaa/movieApi/cache"
 	"github.com/olusolaa/movieApi/db"
 	_ "github.com/olusolaa/movieApi/docs"
 	"github.com/olusolaa/movieApi/server"
@@ -8,11 +9,14 @@ import (
 
 func main() {
 
+	var moviesCache = cache.NewRedisCache("localhost:6379", 1, "", 100)
+
 	DB := &db.PostgresDB{}
 	DB.Init()
 
 	s := &server.Server{
-		DB: DB,
+		DB:    DB,
+		Cache: moviesCache,
 	}
 	s.Start()
 }
