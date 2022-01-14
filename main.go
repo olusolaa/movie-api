00c5e6a804b05be0c6a3b13297d3938957a620e1
Olusola Alao <alaoolusolae@gmail.com>
2022-01-14 17:39:21 +0100

    generate swagger doc

--- main.go
+++ main.go
@@ -7,6 +7,20 @@ import (
 	"github.com/olusolaa/movieApi/server"
 )
 
+// @title        Go + Gin Movie API
+// @version      1.0
+// @description  This is a movie server. You can visit the GitHub repository at https://github.com/olusola/movie-api
+
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  MIT
+// @license.url   https://opensource.org/licenses/MIT
+
+// @host      localhost:8080
+// @BasePath  /
+// @securityDefinitions.basic  BasicAuth
 func main() {
 
 	var moviesCache = cache.NewRedisCache("localhost:6379", 1, "", 100)
--- server/commentHandler.go
+++ server/commentHandler.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/olusolaa/movieApi/models"
 	"log"
@@ -9,10 +10,23 @@ import (
 	"time"
 )
 
+// @Summary Add comments
+// @Description Add comments to a movie by movie id
+// @Accept  json
+// @Produce  json
+// @Param comment body models.CommentRequest true "Comment"
+// @Param movie_id path int true "MovieId"
+// @Success 200 {object} models.Comment
+// @Failure 400 {object} models.Response
+// @Router /api/v1/movies/{movie_id}/comments [post]
 func (s *Server) AddComment() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		movieID, err := strconv.Atoi(c.Param("movie_id"))
-
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid movie id"})
+			return
+		}
 		request := &models.CommentRequest{}
 		if errs := s.decode(c, request); errs != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": errs})
@@ -31,10 +45,44 @@ func (s *Server) AddComment() gin.HandlerFunc {
 			return
 		}
 
+		if !s.increaseCommentCountInRedis(movieID) {
+			log.Println("movie id not found in redis")
+		}
 		c.JSON(http.StatusOK, gin.H{"message": "Comment added successfully!", "data": data})
 	}
 }
 
+// increaseCommentCountInRedis increases the comment count of corresponding movie in redis
+func (s *Server) increaseCommentCountInRedis(movieID int) bool {
+	var movies = s.Cache.Get("movies")
+	if movies != nil {
+		for idx, movie := range *movies {
+			if movie.EpisodeId == movieID {
+				temp := models.Movie{
+					EpisodeId:    movie.EpisodeId,
+					Title:        movie.Title,
+					CommentCount: movie.CommentCount + 1,
+					OpeningCrawl: movie.OpeningCrawl,
+					ReleaseDate:  movie.ReleaseDate,
+				}
+				(*movies)[idx] = temp
+				s.Cache.Set("movies", movies)
+				fmt.Println("movie comment count increased", movies)
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// @Summary Get comments
+// @Description Get all comments for a movie by movie id
+// @Produce  json
+// @Param movie_id path int true "Movie ID"
+// @Success 200 {object} models.Comment
+// @Failure 400 {object} models.Response
+// @Router /api/v1/movies/{movie_id}/comments [get]
+// GetComments returns all comments of a movie
 func (s *Server) GetComments() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		movieId, err := strconv.Atoi(c.Param("movie_id"))
