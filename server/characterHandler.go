239f7f39affd47e4bbecb5e2b52f74a39083eb29
Olusola Alao <alaoolusolae@gmail.com>
2022-01-16 13:03:47 +0100

    add metadata for character response

--- server/characterHandler.go
+++ server/characterHandler.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/olusolaa/movieApi/models"
 	"github.com/olusolaa/movieApi/swapi"
@@ -15,7 +16,7 @@ import (
 // @Description Get all characters for a movie by movie id use the sort parameter
 //to sort the results by name or height or gender, and the order parameter to order in assending or desending order
 // @Produce  json
-// @Param movie_id path string true "Movie ID"
+// @Param movie_id path int true "Movie ID"
 // @QueryParam sort_by query string false "Sort by field"
 // @QueryParam order query string false "Order"
 // @QueryParam filter_by query int false "Filter by field"
@@ -28,13 +29,13 @@ func (s *Server) GetCharacters() gin.HandlerFunc {
 		sortParam := c.Query("sort_by")
 		filterParam := strings.Trim(c.Query("filter"), "\\s+")
 		orderParam := c.Query("order")
-		movieID, err := strconv.Atoi(c.Param("movie_id"))
+		movieId, err := strconv.Atoi(c.Param("movie_id"))
 		if err != nil {
 			log.Println(err)
-			c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid movie id"})
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
-		links, err := swapi.GetAllCharactersByMovieId(movieID)
+		links, err := swapi.GetAllCharactersByMovieId(movieId)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
@@ -87,6 +88,20 @@ func (s *Server) GetCharacters() gin.HandlerFunc {
 			}
 			characters = filteredList
 		}
-		c.JSON(http.StatusOK, gin.H{"message": "user info retrieved successfully", "data": characters})
+		heightTotal := float64(0)
+		for _, character := range characters {
+			height, err := strconv.ParseFloat(character.Height, 64)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			heightTotal += height
+		}
+		ft := heightTotal / 34
+		inches := heightTotal / 24.53
+		heightString, _ := fmt.Printf("%.2fcm | %2fft | %2finches", heightTotal, ft, inches)
+
+		c.JSON(http.StatusOK, gin.H{"message": "user info retrieved successfully", "data": characters,
+			"metadata": gin.H{"matching_characters": len(characters), "total_height": heightString}})
 	}
 }
